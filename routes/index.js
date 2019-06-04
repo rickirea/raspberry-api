c011f437d2490842d6230592d102d10e49333d3e
Ricardo Rea <rickirea@gmail.com>
2019-06-04 01:05:23 -0500

    Handling unexport when it finished to blink

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,20 @@ router.get('/', (req, res, next) => {
 /* POST Blink Test */
 router.post('/blink', (req, res, next) => {
   try{
-    let blinkInterval = setInterval(blinkLED, 250);
+    let blinkInterval = setInterval(()=>{
+      if (LED.readSync() === 0) { //check the pin state, if the state is $
+        LED.writeSync(1); //set pin state to 1 (turn LED on)
+      } else {
+        LED.writeSync(0); //set pin state to 0 (turn LED off)
+      }
+    }, 250);
+    
     setTimeout(() => {
       clearInterval(blinkInterval); // Stop blink intervals
       LED.writeSync(0); // Turn LED off
-      LED.unexport(); // Unexport GPIO to free resources
+      //LED.unexport(); // Unexport GPIO to free resources
     }, 5000);
+    
     res.status(200).json({message: "Success!!!"});
   }
   catch(err){
